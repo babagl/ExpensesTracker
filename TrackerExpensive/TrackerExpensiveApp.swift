5f53a73b96126fa026650a265337b3cef6a2cc1b
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-04-05 08:17:33 +0000

    Initial Commit

---
+++ TrackerExpensive/TrackerExpensiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerExpensiveApp.swift
+//  TrackerExpensive
+//
+//  Created by Abdoulaye Aliou SALL on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrackerExpensiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
